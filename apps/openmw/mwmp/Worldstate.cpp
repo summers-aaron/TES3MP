741553dc0062955af90d0b5d12bd5a5deddc2c2b
David Cernat <davidcernat@gmail.com>
2020-08-11 18:33:12 +0200

    [Client] Fix spacing for sound records in mwmp::Worldstate

--- apps/openmw/mwmp/Worldstate.cpp
+++ apps/openmw/mwmp/Worldstate.cpp
@@ -318,15 +318,15 @@ void Worldstate::addRecords()
     }
     else if (recordsType == mwmp::RECORD_TYPE::SOUND)
     {
-    for (auto&& record : soundRecords)
-    {
-        bool hasBaseId = !record.baseId.empty();
+        for (auto&& record : soundRecords)
+        {
+            bool hasBaseId = !record.baseId.empty();
 
-        LOG_APPEND(TimedLog::LOG_INFO, "- sound record %s\n-- baseId is %s", record.data.mId.c_str(),
-            hasBaseId ? record.baseId.c_str() : "empty");
+            LOG_APPEND(TimedLog::LOG_INFO, "- sound record %s\n-- baseId is %s", record.data.mId.c_str(),
+                hasBaseId ? record.baseId.c_str() : "empty");
 
-        RecordHelper::overrideRecord(record);
-    }
+            RecordHelper::overrideRecord(record);
+        }
     }
 }
 
